d8b7e5710769db5a1d3227459343dcab5748d635
Brandon <brandon@parksbra.com>
2025-12-01 19:04:30 -0800

    update namespace var

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,10 +1,10 @@
 resource "kubernetes_secret" "cert_manager_dns_solver_api_token" {
   depends_on = [
-    data.kubernetes_namespace.cert_manager_namespace
+    data.kubernetes_namespace.cert_manager
   ]
   metadata {
     name      = "dns-solver-api-token-secret"
-    namespace = data.kubernetes_namespace.cert_manager_namespace.metadata[0].name
+    namespace = data.kubernetes_namespace.cert_manager.metadata[0].name
   }
   data = {
     "${local.cert_manager_dns_solver_secret_key}" = var.cert_manager_dns_solver_api_token
